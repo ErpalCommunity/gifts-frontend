ce0392eaacd08eb13f59a3f11b7910c296fa69db
ErpalCommunity <factspocta@gmail.com>
2024-11-04 17:25:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navigator from './Navigator';
 import Store from './Store';
 import './App.css'; 
+import giftTitle from './gift-title.png'; // Убедитесь, что путь правильный
 
 function App() {
+  const [imgError, setImgError] = useState(false); // Состояние для отслеживания ошибки изображения
+
   return (
     <Router>
       <div className="App">
         <div className="titles">
-          <img src="/gift-title.png" className="image-title" alt="Gift" crossOrigin="anonymous" />
+          {imgError ? (
+            <p className="error-message">Ошибка: изображение не загрузилось.</p>
+          ) : (
+            <img 
+              src={giftTitle} 
+              className="image-title" 
+              alt="Gift" 
+              onError={() => setImgError(true)} // Обработчик ошибки загрузки
+            />
+          )}
           <p className="text-title">Buy and Send Gifts</p>
           <p className="text-description">Unique gifts for everyone by Crypto Pay.</p>
         </div>
