6360039ed50394407d741b9ad3fc79b924f13651
ErpalCommunity <factspocta@gmail.com>
2024-11-05 22:38:26 +0300

    initial commit

--- src/cards/usdt.js
+++ src/cards/usdt.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './usdt.css';
 
 const UsdtPage = () => {
+  useEffect(() => {
+    // Показываем кнопку "Назад"
+    if (window.Telegram) {
+      window.Telegram.WebApp.onBackButtonPressed(() => {
+        // Здесь вы можете определить, что делать при нажатии на кнопку "Назад"
+        console.log('Back button pressed');
+        // Например, можно вернуться на предыдущую страницу или закрыть мини-приложение
+        // window.history.back(); // Вернуться на предыдущую страницу
+        window.Telegram.WebApp.close(); // Закрыть мини-приложение
+      });
+      window.Telegram.WebApp.setupBackButton(); // Отображаем кнопку "Назад"
+    }
+  }, []);
+
   return (
     <div className="buy-container">
       <div className="img-buy">
